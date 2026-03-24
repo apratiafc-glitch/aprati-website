bfb4ecfb07e88945aa0869346218f8a6d749881d
apratiafc-glitch <aprati.afc@gmail.com>
2026-03-24 16:34:12 +0700

    fix: update CORS for production domain

--- aprati_backend/config/cors.php
+++ aprati_backend/config/cors.php
@@ -24,6 +24,7 @@ return [
         'http://localhost:3001',
         'http://127.0.0.1:3000',
         'http://127.0.0.1:3001',
+        'https://sdev.apratifoods.asia',
     ],
 
     'allowed_origins_patterns' => [],
